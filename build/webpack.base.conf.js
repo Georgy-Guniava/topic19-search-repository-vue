3608b4a61b8eba47a46f389a2433c34f7a682bc7
Georgy Guniava <guniava.gg@gmail.com>
2018-10-09 13:11:38 +0300

    fix

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -41,23 +41,12 @@ module.exports = {
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
-        test: /\.(png|jpe?g|gif)(\?.*)?$/,
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
-      },
-      {
-        test: /\.(svg)(\?.*)?$/,
-        loader: 'svgo-loader',
-        options: {
-          plugins: [
-            {removeTitle: true},
-            {convertColors: {shorthex: false}},
-            {convertPathData: false}
-          ]
-            }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
