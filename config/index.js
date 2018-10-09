b32bafad583161fdc109d452048963fed943d411
Georgy Guniava <guniava.gg@gmail.com>
2018-10-09 10:44:30 +0300

    test

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/topic19-search-repository-vue/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/topic19-search-repository-vue/',
 
     /**
      * Source Maps
